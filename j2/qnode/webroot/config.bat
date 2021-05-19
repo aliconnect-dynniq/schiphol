775f679f37709996bcf9b2ecaba68f894d30bf56
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-19 10:22:00 +0200

    Auto commit

--- j2/qnode/webroot/config.bat
+++
@@ -1,3 +0,0 @@
-SET id=3682790
-SET uid=3525a378-7a10-4418-9dda-08ebe2557e8f
-SET root="\aim\www"
--- j2/qnode/webroot/run_em.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:node
-cls
-node dms --config="config.json" --loglevel=5
-pause
-GOTO node
